c829f928dd4c8271b55e7bedd4cc8a56ea11228e
Johnson <johnson.he61@gmail.com>
2020-08-29 17:40:52 -0500

    finish1

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
   $(window).scroll(function(){
-    if(this.scrollY > 20) 
+    if(this.scrollY > 20){
       $(".navbar").addClass("sticky");
-    else
+      $(".goTop").fadeIn();
+    }
+    else{
       $(".navbar").removeClass("sticky");
+      $(".goTop").fadeOut();
+    }
   });
+
+  $(".goTop").click(function(){scroll(0,0)});
 
   $('.menu-toggler').click(function(){
     $(this).toggleClass("active");
     $(".navbar-menu").toggleClass("active");
   });
+
+  
+
+  $('.item-wrap a').magnificPopup({
+
+    type:'inline',
+    fixedContentPos: false,
+    removalDelay: 200,
+    showCloseBtn: false,
+    mainClass: 'mfp-fade'
+
+ });
+
+ $(document).on('click', '.popup-modal-dismiss', function (e) {
+     e.preventDefault();
+     $.magnificPopup.close();
+ });
+
 });
